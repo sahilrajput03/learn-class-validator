8eb168890c90af1ad751678594b697ab89b83d1a
Sahil Rajput <sahilrajput03@gmail.com>
2022-11-19 00:26:40 +0530

    up

--- 1.ts
+++ 1.ts
@@ -29,7 +29,20 @@ let sample2 = new ChatMessage()
 sample2.message = ''
 sample2.toUserId = ''
 
-const [validation1, validation2] = validateSync(sample2).map((e) => Object.values(e.constraints as any)[0])
+const validations = validateSync(sample2)
+let [validation1, validation2] = validations
 
-expect(validation1).toBe('message should not be empty')
-expect(validation2).toBe('toUserId should not be empty')
+const m1 = `An instance of ChatMessage has failed the validation:
+ - property message has failed the following constraints: isNotEmpty`
+const m2 = `An instance of ChatMessage has failed the validation:
+ - property toUserId has failed the following constraints: isNotEmpty`
+
+expect(validation1.toString().trimEnd()).toBe(m1)
+expect(validation2.toString().trimEnd()).toBe(m2)
+
+let [messg1, messg2] = validations.map((e) => Object.values(e.constraints as any)[0])
+
+expect(messg1).toBe('message should not be empty')
+expect(messg2).toBe('toUserId should not be empty')
+
+console.log('Reached end of file.')
